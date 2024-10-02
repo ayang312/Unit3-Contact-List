42bb8198679f045833dbf8dbc959f7a13d633cef
Andrew Yang <ayang312@gmail.com>
2024-10-01 20:52:56 -0400

    created contact list and row components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ContactList from "./components/ContactList";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <ContactList />
     </>
-  )
+  );
 }
-
-export default App
