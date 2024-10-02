6ee7c935d635df0a2d9ad109add9a5b93ebfc9fa
Andrew Yang <ayang312@gmail.com>
2024-10-01 21:19:22 -0400

    completed workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import ContactList from "./components/ContactList";
+import { useState } from "react";
+import SelectedContact from "./components/SelectedContact";
 import "./App.css";
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <SelectedContact
+          contactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
---
+++ src/components/SelectedContact.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export default function SelectedContact({ contactId, setSelectedContactId }) {
+  const [contact, setContact] = useState(null);
+
+  useEffect(() => {
+    async function fetchContact() {
+      try {
+        const response = await fetch(
+          `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${contactId}`
+        );
+        const result = await response.json();
+        setContact(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContact();
+  }, [contactId]);
+
+  if (!contact) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{contact.name}</h2>
+      <p>Email: {contact.email}</p>
+      <p>Phone: {contact.phone}</p>
+      <button onClick={() => setSelectedContactId(null)}>
+        Back to Contact List
+      </button>
+    </div>
+  );
+}
